c63603344221b31cd80c3fa6e17364054ba0b5b6
LuizLacerdaa <luizlacerda.r@gmail.com>
2025-06-09 19:49:11 -0300

    Initial commit

---
+++ filmes/index.php
@@ -0,0 +1,42 @@
+<?php
+include "cabecalho.php";
+include "banner.php";
+?>
+<div class="container">
+    <h2 class="display-5">Melhores avaliações</h2>
+ 
+    <div class="row">
+        <div class="col-3">
+            <img src="img/filme1.webp" class="img-fluid">
+            <h3>Jurassic Park</h3>
+            <span>⭐ 10/10</span>
+        </div>
+        <div class="col-3">
+            <img src="img/filme2.jpg" class="img-fluid">
+            <h3>Karate Kid Legends</h3>
+            <span>⭐ 10/10</span>
+        </div>
+        <div class="col-3">
+            <img src="img/filme3.jpg" class="img-fluid">
+            <h3>Lilo e Stitch</h3>
+            <span>⭐ 10/10</span>
+        </div>
+        <div class="col-3">
+            <img src="img/filme4.jpg" class="img-fluid">
+            <h3>Pecadores</h3>
+            <span>⭐ 10/10</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-8">
+            <img src="img/banner.jpg" class="img-fluid">
+        </div>
+        <div class="col-4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus non velit officia, placeat illum beatae quia modi perspiciatis ex ratione! Modi nostrum enim perferendis nulla saepe veritatis nemo dolorem rerum!
+            <a href="https://youtube.com" class="btn btn-success btn-lg">ASSISTA AO TRAILER AGORA</a>
+            </p>
+        </div>
+ 
+    </div>
+</div>
+    <?php include "rodape.php"; ?>
